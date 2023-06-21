e1a09d84b565dbc370459cd918a9546390d6398c
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2023-06-22 05:28:31 +0530

    Added cron job and env variables

--- server/index.ts
+++ server/index.ts
@@ -2,15 +2,20 @@ import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
-import morgan from 'morgan';
-import { PORT } from './config/config'
+import { PORT, server_url } from './config/config'
 import authUser from './routes/authUser'
 import authMiddleware from './middleware/authMiddleware'
 import getSocietyData from './utils/getSocietyData'
 import getChartData from './utils/getChartData'
 import connToDB from './config/db';
+import cron from 'node-cron'
+import https from 'https'
+
 const app: Application = express();
- 
+app.get('/', (req: any, res: any) => {
+  console.log('Request arrived', server_url)
+  res.status(200).send('thanks')
+})
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
@@ -21,6 +26,13 @@ connToDB()
 app.use('/auth/',authUser)
 app.use('/get/', getSocietyData)
 app.use('/chart/', getChartData)
+cron.schedule('*/1 * * * *', () => {
+  console.log('stay awake')
+  // https.get(server_url, (res: any) => {
+  //   res.on('data', (data: any) => console.log(data))
+  //   res.on('end',()=>console.log('Ended'))
+  // }).on('error',(error:Error)=>console.log(error))
+});
 app.listen(PORT, () => {
   console.log(`Server Fired up, on port ${PORT}` );
 });
