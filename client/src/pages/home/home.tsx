50dc8b10a64610441418ef7a0abbd471ddb3d94e
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-06-24 12:00:30 +0530

    Added build folder

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
-import Navigator from '../../components/dashboard/Navigator'
 import { Box } from '@mui/material'
 import AppNavig from '../../components/dashboard/appNavig'
 
@@ -8,7 +7,6 @@ const Home = () => {
   return (
     <Box>
       <Navbar/>
-      {/* <Navigator /> */}
       <AppNavig/>
     </Box>
   )
