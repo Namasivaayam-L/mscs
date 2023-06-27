0d0b1f7f0d99ae0f3099fe65638e610d26c502c1
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-06-27 16:00:09 +0530

    Update config.ts

--- server/config/config.ts
+++ server/config/config.ts
@@ -2,5 +2,7 @@ require('dotenv').config()
 export const PORT: Number = 5000
 export const secret: string = 'test'
 // export const mongoURI: string = 'mongodb://127.0.0.1:27017/mscs'
-export const mongoURI: any = process.env.MONGODB_URI
-export const server_url:any = process.env.NODEJS_SERVER
+// export const mongoURI: any = process.env.MONGODB_URI
+// export const server_url:any = process.env.NODEJS_SERVER
+export const server_url:any = 'https://mscs-server.onrender.com'
+export const mongoURI: any = ''
