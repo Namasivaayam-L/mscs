8df1ac88da407aa09f3cef57176d1f5c36814991
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-06-27 15:59:02 +0530

    Update config.ts

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,5 @@
 export const verifyCaptcha = true
-export const client_url = process.env.REACT_APP_CLIENT
-export const server_url = process.env.REACT_APP_SERVER
+// export const client_url = process.env.REACT_APP_CLIENT
+export const client_url = 'https://mscs-competition-namasivaayam-l.netlify.app'
+// export const server_url = process.env.REACT_APP_SERVER
+export const server_url = 'https://mscs-server.onrender.com'
