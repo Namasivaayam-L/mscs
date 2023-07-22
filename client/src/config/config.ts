4e7932dd5cb1b259ff55d83537a8f3605e6b5eab
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-07-22 12:20:12 +0530

    Backend server down

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,5 +1,8 @@
 export const verifyCaptcha = true
 // export const client_url = process.env.REACT_APP_CLIENT
 export const client_url = 'https://mscs-competition-namasivaayam-l.netlify.app'
+// export const client_url = 'http://localhost:3000'
 // export const server_url = process.env.REACT_APP_SERVER
-export const server_url = 'https://mscs-server.onrender.com'
+export const server_url = 'https://mscsc-server.onrender.com'
+// export const server_url = 'http://localhost:5000'
+
