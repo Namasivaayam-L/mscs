87e193cdfce8a7da99e459d4743ce3f8617325f0
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-07-29 21:08:34 +0530

    Removed snackbar bug and added contact

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -16,25 +16,25 @@ interface State extends SnackbarOrigin {
 }
 const Home = () => {
   const [state,] = React.useState<State>({
-    open: false,
-    vertical: "top",
-    horizontal: "center",
+    open: true,
+    vertical: "bottom",
+    horizontal: "right",
   });
   const { vertical, horizontal, open } = state;
   return (
     <Box>
       <Navbar/>
       <AppNavig/>
-      <Snackbar open={open} autoHideDuration={100000} anchorOrigin={{ vertical, horizontal }}>
+      <Snackbar open={open} autoHideDuration={100000} >
         <Alert severity="error" sx={{ width: '100%' }}>
-          If you don't see any visualization, it means the free hosting server is gone into sleep mode,
+          If you don't see any visualization, it means the free hosting server is gone into sleep mode,<br/ >
           please wait for 30 seconds-1 minute and refresh or switch tabs in visualization.
         </Alert>
       </Snackbar>
-      <Snackbar open={open} autoHideDuration={100000}>
-        <Alert severity="success" sx={{ width: '100%' }}>
-          If you can't see any visualization even after 2 minutes,
-          please alert me through this mail-id : <a href="mailto:namasivaayam007vijay_ai@mepcoeng.ac.in">namasivaayam007vijay_ai@mepcoeng.ac.in</a>
+      <Snackbar open={open} autoHideDuration={100000} anchorOrigin={{ vertical, horizontal }}>
+        <Alert severity="success" sx={{ width: '100%' }} >
+          If you can't see any visualization even after 2 minutes,please alert me <br/>
+          through this mail-id : <a href="mailto:namasivaayam007vijay_ai@mepcoeng.ac.in">namasivaayam007vijay_ai@mepcoeng.ac.in</a>
           Thank you so much.
         </Alert>
       </Snackbar>
--- server/config/config.ts
+++ server/config/config.ts
@@ -2,7 +2,6 @@ require('dotenv').config()
 export const PORT: Number = 5000
 export const secret: string = 'test'
 // export const mongoURI: string = 'mongodb://127.0.0.1:27017/mscs'
-// export const mongoURI: any = process.env.MONGODB_URI
-// export const server_url:any = process.env.NODEJS_SERVER
-export const server_url:any = 'https://mscs-server.onrender.com'
-export const mongoURI: any = 'mongodb+srv://namas-mscs:namas-mscs@cluster0.zaxrt3p.mongodb.net/mscs?retryWrites=true&w=majority'
+export const mongoURI: any = process.env.MONGODB_URI
+export const server_url:any = process.env.NODEJS_SERVER
+
