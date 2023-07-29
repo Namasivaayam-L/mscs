dc0c6d2d88d5ab0fdedee8fcaa2590f1da908e34
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-07-29 20:41:17 +0530

    Added Contact mail in snackbar

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -14,7 +14,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 
 
 const Home = () => {
-  const [open, setOpen] = React.useState(true);
+  const [open,] = React.useState(true);
   return (
     <Box>
       <Navbar/>
@@ -23,6 +23,13 @@ const Home = () => {
         <Alert severity="error" sx={{ width: '100%' }}>
           If you don't see any visualization, it means the free hosting server is gone into sleep mode,
           please wait for 30 seconds-1 minute and refresh or switch tabs in visualization.
+        </Alert>
+      </Snackbar>
+      <Snackbar open={open} autoHideDuration={100000}>
+        <Alert severity="success" sx={{ width: '100%' }}>
+          If you can't see any visualization even after 2 minutes,
+          please alert me through this mail-id : <a href="mailto:namasivaayam007vijay_ai@mepcoeng.ac.in">namasivaayam007vijay_ai@mepcoeng.ac.in</a>
+          Thank you so much.
         </Alert>
       </Snackbar>
     </Box>
